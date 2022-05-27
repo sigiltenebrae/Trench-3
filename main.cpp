0c5f905dbae5082f3e63b5f6a66a56c14de293f6
chris <christian.kobb@gmail.com>
2022-05-27 14:45:59 -0400

    opencv set up

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
 
 int main() {
-    std::cout << "Hello, World!, testing" << std::endl;
+
     return 0;
 }
